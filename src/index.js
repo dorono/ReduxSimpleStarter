149e321fe81c3726f507f196168efacfb180d3a5
Doron Orenstein <jazziseverywhere@gmail.com>
2016-12-16 08:00:01 -0600

    sec, lecture 31 completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -7,6 +8,7 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBVFyv6UJcXQIEFCmrXSnkQerhqPTHn3qA';
 
+// a class is used whenever you need to set state
 class App extends Component {
   constructor (props) {
     super(props);
@@ -29,10 +31,13 @@ class App extends Component {
     });
   }
 
+
   render () {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
