f61a6447342ee93ce2107aebebca07761958aa78
Doron Orenstein <jazziseverywhere@gmail.com>
2016-12-19 21:52:04 -0600

    continued work...

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
-export const ROOT_URL = 'http://reduxblog.heroku/api';
-export const API_KEY = '?key=12532tg35d24';
+export const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+export const API_KEY = '?key=123';
 
 export function fetchPosts() {
-  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
-
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+  console.log('request', request);
   return {
     type: FETCH_POSTS,
-    payload: action.request
+    payload: request
   };
 }
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,31 @@
-import React from 'react';
+// PROCESS FOR CREATING A CONTAINER THAT CAN CALL ACTION CREATORS
+// 1) Import connect at top
+// 2) Import action creator
+// 3) We define our mapDispatchToProps() function
+// 4) And we connect it to our component
 
-export default () => {
-  return <div>List of blog posts</div>;
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
+
+
+class PostsIndex extends Component {
+  // componentWillMount() is a LIFECYCLE METHOD only called ONCE, just when component is
+  // initially loaded to the DOM.
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render () {
+    return(
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchPosts}, dispatch);
+}
+
+export default connect(null, {fetchPosts})(PostsIndex);
